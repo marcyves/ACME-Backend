8031d8f8af5d0b3e0cece5328fb82e57aa97558b
Marc Augier <marcyves@users.noreply.github.com>
2022-05-14 22:53:11 +0200

    orthographe

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -12,6 +12,6 @@ module.exports = ( req, res, next ) => {
             next();
         }
     } catch ( error ) {
-        res.status( 401 ).json( { error: error | 'non authenttifié' } );
+        res.status( 401 ).json( { error: error | 'non authentifié' } );
     }
 };
